497b31753cba42f824f91b31d7787c0576800182
Gonçalo Cruz Moreira <cruzmoreira1990@gmail.com>
2024-01-11 19:03:31 +0000

    ball

--- src/ExperienceBall.js
+++ src/ExperienceBall.js
@@ -1,15 +1,15 @@
 import React from "react";
-
 function ExperienceBall({ x, y }) {
   return (
-    <div
+    <img
+      src="/exp.png" // path relative to the public folder
+      alt="Experience Ball"
       style={{
         position: "absolute",
         left: `${x}px`,
         top: `${y}px`,
-        width: "10px",
-        height: "10px",
-        backgroundColor: "green",
+        width: "50px",
+        height: "50px",
       }}
     />
   );
