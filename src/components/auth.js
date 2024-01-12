e8a41f63db360c0274fbf018ba35a8943cbc7d67
Francisco <franciscoliveira7777@gmail.com>
2024-01-12 09:33:00 +0000

    final commit from me

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,26 +1,31 @@
 import '../styles/auth.css'
 import { auth, googleProvider } from '../config/firebase'
 import { signInWithPopup, signOut } from 'firebase/auth'
+import { useState } from 'react'
 
 export const Auth = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
   const signIn = async () => {
     try {
       await signInWithPopup(auth, googleProvider)
+      setIsLoggedIn(true)
     } catch (error) {
       console.log(error)
     }
   }
-  const logout = async () => {
+  const signout = async () => {
     try {
       await signOut(auth)
+      setIsLoggedIn(false)
     } catch (error) {
       console.log(error)
     }
   }
 
   return auth.currentUser != null ? (
-    <button className="btn btn-auth" onClick={logout}>
-      Logout
+    <button className="btn btn-auth" onClick={signout}>
+      Sign Out
     </button>
   ) : (
     <button className="btn btn-auth" onClick={signIn}>
